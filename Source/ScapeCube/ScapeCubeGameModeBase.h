2651ffab2128350dc3fa2b3077c1d74a70fa7c71
lucianodiaz <lucianodiazberberich@gmail.com>
2022-01-03 18:20:39 -0300

    FirstCommit

---
+++ Source/ScapeCube/ScapeCubeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ScapeCubeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCAPECUBE_API AScapeCubeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
